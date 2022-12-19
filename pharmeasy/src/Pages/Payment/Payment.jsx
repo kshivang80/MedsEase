dc8961304eb0a2b8d8483c58a8414a3f0ee66331
your_name_here <your_email@email.com>
2022-12-19 12:08:21 +0530

    changes

--- pharmeasy/src/Pages/Payment/Payment.jsx
+++ pharmeasy/src/Pages/Payment/Payment.jsx
@@ -3,14 +3,24 @@ import "./payment.css"
 import { Button,Radio,RadioGroup,Alert,AlertIcon,AlertTitle,AlertDescription } from '@chakra-ui/react'
 import {ChevronRightIcon} from "@chakra-ui/icons"
 
+let data=JSON.parse(localStorage.getItem("checkout-arr"))||[];
 const Payment = () => {
   const [rad, setRad]=useState(false)
+  let sum=0;
+ 
+  if(data.length!==0){
+    const totalcartprice=data.map((item)=>{
+      return Number(item.mrpDecimal-item.discountDecimal)
+    })
+    for(let i=0;i<totalcartprice.length;i++){
+      sum+=totalcartprice[i];
+    }
+  }
 
   const handlePayment=()=>{
-
-    console.log("inside handlepayment");
+    
+    console.log("inside handlepayment");    
    alert("payment successful");
-
   }
   return (
     <div>
@@ -158,7 +168,7 @@ const Payment = () => {
             <div id="amountdesc">
               <div className='cartvalue'>
               <div><h1>Cart Value</h1></div>
-              <div>{429}</div>
+              <div>{sum}</div>
               </div>
               <div className='cartvalue'>
                   <div><h1>Delivery charges</h1></div>
@@ -168,7 +178,7 @@ const Payment = () => {
             <div id='totalpaidamount'>
               <div className='cartvalue'>
                   <div><h1>Amount to be paid</h1></div>
-                  <div>408.54</div>
+                  <div>{sum+129}</div>
               </div>
               
             </div>

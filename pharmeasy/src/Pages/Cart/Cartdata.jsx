d4f01801b09b96cc1e89717573027eebb5328eec
Shivang kumar <103144321+kshivang80@users.noreply.github.com>
2023-01-29 21:12:06 +0530

    Cart-page alignment css done

--- pharmeasy/src/Pages/Cart/Cartdata.jsx
+++ pharmeasy/src/Pages/Cart/Cartdata.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { DeleteIcon } from '@chakra-ui/icons'
 import { useNavigate, useParams } from 'react-router-dom';
-import { Button } from '@chakra-ui/react';
+import { Button, Image } from '@chakra-ui/react';
 
 // const temp = JSON.parse(localStorage.getItem('checkout-arr') || '[]');
 const Cartdata = ({ data }) => {
@@ -24,19 +24,20 @@ const Cartdata = ({ data }) => {
                 data.map((elm) => {
 
                     return <div key={elm.productId} >
-                        <div className='prod' style={{ display: "flex" }}>
-                            <div className='prodimg' style={{ width: "10%" }}>
-                                <img src={elm.images} alt="" />
+                        <div className='prod' style={{ display: "flex",marginTop:"15px" }}>
+                            <div className='prodimg' style={{ width: "18%",height:"100px" ,boxShadow: "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px"}}>
+                                <Image w='90%' h='90%' m='auto' marginTop="5px"  src={elm.images} alt="" />
                             </div>
-                            <div className='proddetails' style={{ width: "90%" }}>
+                            <div className='proddetails' style={{ width: "90%", }}>
                                 <div style={{ display: "flex", justifyContent: "space-between" }}>
-                                    <div><h2 style={{ color: 'black', fontWeight: "bold" }}>{elm.name}</h2></div>
+                                    <div style={{marginLeft:"15px",marginTop:"5px"}}><h2 style={{ color: 'black', fontWeight: "bold" }}>{elm.name}</h2></div>
                                     <div><DeleteIcon onClick={() => { handleDelete(elm.productId) }} /></div>
                                 </div>
 
-                                <p style={{ color: 'gray' }}>{data[0].manufacturer}</p>
-                                <h2 style={{ color: 'black', fontWeight: "bold" }}>MRP.{data[0].mrpDecimal - data[0].discountDecimal}</h2>
+                                <p style={{ color: 'gray' ,marginLeft:"15px"}}>{data[0].manufacturer}</p>
+                                <h2 style={{ color: 'black', fontWeight: "bold",marginLeft:"15px" }}>MRP.{data[0].mrpDecimal - data[0].discountDecimal}</h2>
                             </div>
+                           
                         </div>
                     </div>
 

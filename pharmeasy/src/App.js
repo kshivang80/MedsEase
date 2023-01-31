699940fc9c66124a6bcb6666f3636aa9d6cb8132
Suman Dhull <sumandhull@Sumans-Air.domain.name>
2023-01-31 13:29:42 +0530

    offerPage

--- pharmeasy/src/App.js
+++ pharmeasy/src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
        <Navbar/>
-      {/* <AllRouter/> */}
-      <OfferItem/>
+      <AllRouter/>
+      {/* <OfferItem/> */}
        <Footer/>
     </div>
   );

82606831952fa81586ce180344fecd8871d9f64a
Suman Dhull <sumandhull@Sumans-MacBook-Air.local>
2023-01-31 12:40:31 +0530

    offerPage

--- pharmeasy/src/Pages/Offers/OfferItem.jsx
+++ pharmeasy/src/Pages/Offers/OfferItem.jsx
@@ -1,21 +1,27 @@
 import { Box, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import styles from "./OfferItems.module.css"
+import {IoIosArrowForward} from "react-icons/io"
 
 export default function OfferItem() {
   return (
-    <Box>
-          <Box>
+    <Box className={styles.offerItem}>
+          <Box className={styles.offerItem1}>
               <Box>
-                  <Image src="" alt="" />
+                  <Image src={Data.img} alt="" />
               </Box>
               <Box>
-                  <Text as="p"></Text>
-                  <Text as="p"></Text>
-              </Box>
+          <Text as="p">{Data.title}</Text>
+          <Text as="p">{Data.content}</Text>
+        </Box>
+        <Box width="fit-content"><IoIosArrowForward className={styles.icon} size={25} fill="teal"/></Box>
       </Box>
-          <Box>
-              <Text as="p"></Text>
-              <Text as="p"></Text>
+      <Box borderBottom="1px dashed"></Box>
+          <Box className={styles.offerItem2}>
+        {Data.code==="No Code Required" ?<><Text as="p">{Data.code}</Text>
+              <Text as="p">Continue</Text></>: 
+        <><Text as="p">Code: <span style={{color:"black", fontWeight:700}}>{Data.code}</span></Text>
+              <Text as="p">Copy Code</Text></>}
       </Box>
     </Box>
   )
